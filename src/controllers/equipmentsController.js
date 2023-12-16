fdee6b19c0766e411e1acc8109d4f4489d34ed7c
sarAmgad <sarahelmy56@gmail.com>
2023-12-16 20:40:25 +0200

    Clinic database

--- src/controllers/equipmentsController.js
+++ src/controllers/equipmentsController.js
@@ -1,30 +1,83 @@
 const { PrismaClient } = require('@prisma/client');
 const equipmentsClient= new PrismaClient();
 
-// Add new equipments to the database
-const addEquipments = async(req,res)=>{
-    try{
-        const equipmentData= req.body;
-        const equipment= await equipmentsClient.equipments.create({
-            data:equipmentData,
-     });
-        res.status(200).json({message: "success"});
-    }catch(err){
-        res.status(500).json({ message: err.message });
-    }
-};
-
 // get all Equipments in clinic
  const getAllEquipments = async(req,res)=>{
     try{
         const allEquipments= await equipmentsClient.equipments.findMany();
+        
         res.status(200).json({allEquipments});
-    }catch(err){
-        res.status(500).json({ message: err.message });
+    }catch(error){
+        res.status(500).json({ message: error.message });
+    }
+};
+
+//Get Equipment by ID
+const getEquipmentById = async( req, res) => {
+    try {
+        const post = await equipmentsClient.equipments.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        
+        res.status(200).json({ post });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Add new equipments to the database
+const addEquipments = async(req,res)=>{
+    try{
+        const equipmentData= req.body;
+
+        const equipment = await equipmentsClient.equipments.create({
+            data: equipmentData,
+        });
+       
+        res.status(200).json({equipment});
+    }catch(error){
+        res.status(500).json({ message: error.message });
+    }
+};
+
+//Update Equipment
+const updateEquipment = async (req, res) => {
+    try {
+        const equipmentData= req.body;
+        const post = await equipmentsClient.equipments.update({
+            where:{
+                id: Number(req.params.id),
+            },
+            data: equipmentData,
+        });
+ 
+        res.status(200).json({ post }); 
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+//Delete Equipment
+const deleteEquipment = async (req, res) => {
+    try {
+        await equipmentsClient.equipments.delete({
+            where: {
+                id: Number (req.params.id)
+            }
+        });
+
+        res.status(200).json({message: "Equipment Deleted"});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
 
 module.exports = {
     getAllEquipments,
-    addEquipments
-  };
+    addEquipments,
+    getEquipmentById,
+    updateEquipment,
+    deleteEquipment
+};
--- src/routes/equipmentsRouter.js
+++ src/routes/equipmentsRouter.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router   = express.Router();
 const equipmentsController = require("../controllers/equipmentsController");
 
-router.get('/',equipmentsController.getAllEquipments );
-router.post('/',equipmentsController.addEquipments );
+router.get('/', equipmentsController.getAllEquipments);
+router.get('/:id', equipmentsController.getEquipmentById);
+router.post('/', equipmentsController.addEquipments);
+router.put('/:id', equipmentsController.updateEquipment);
+router.delete('/:id', equipmentsController.deleteEquipment);
+
 module.exports = router;
