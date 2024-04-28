c066e4d2bd4455f113b33e9ee846129b658b928e
Rawda-Yousry <rawda.ahmed02@eng-st.cu.edu.eg>
2024-04-28 18:49:17 +0300

    eslint-disable_features

--- Service/regestiration/src/Scripts/hashing.ts
+++ Service/regestiration/src/Scripts/hashing.ts
@@ -1,28 +1,29 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable no-useless-catch */
 import bcrypt from "bcrypt";
 
 const hashPassword = async (password: string) => {
-    //hash password using bcrypt package
-    const salt = await bcrypt.genSalt(10);
-    password = await bcrypt.hash(password, salt);
-    return password
+  //hash password using bcrypt package
+  const salt = await bcrypt.genSalt(10);
+  password = await bcrypt.hash(password, salt);
+  return password;
+};
 
-}
-
-const compareHashPassword = async (password: string, existPassword: string, error: any) => {
-    // compare password w/ hashed password in database using bcrypt package
-    try {
-        const validPass = await bcrypt.compare(password, existPassword);
-        if (!validPass) {
-            throw error
-        }
-    } catch (error) {
-        throw error
+const compareHashPassword = async (
+  password: string,
+  existPassword: string,
+  error: any
+) => {
+  // compare password w/ hashed password in database using bcrypt package
+  try {
+    const validPass = await bcrypt.compare(password, existPassword);
+    if (!validPass) {
+      throw error;
     }
-}
+  } catch (error) {
+    throw error;
+  }
+};
 
 export default {
-    hashPassword,
-    compareHashPassword
-}
+  hashPassword,
+  compareHashPassword,
+};
