d2c2b1df8f11c722d42828f40e697f00cde95d19
SamaMosatafa1 <samamostafa789@gmail.com>
2024-05-02 09:03:40 +0200

    edits

--- Service/regestiration/src/hl7/sendM.js
+++ Service/regestiration/src/hl7/sendM.js
@@ -6,7 +6,7 @@ async function sendM(message) {
   return new Promise((resolve, reject) => {
     // Server connection
     var client = hl7.Server.createTcpClient({
-      host: '127.0.0.1',
+      host: '45.247.123.161',
       port: 3888,
       keepalive: true,
       callback: async function(err, ACK) {
@@ -16,6 +16,9 @@ async function sendM(message) {
           reject(err); // Reject the Promise with the error
         } else {
           console.log(ACK.log() + "========= " );
+          console.log("sssssssss",ACK.header.getComponent(7, 1).toString())
+          console.log("sssssssssaaaaaa")
+          if(ACK.header.getComponent(7, 1).toString()== "ADR"){
           var diagnosis = [] ;
 
           ACK.segments.forEach(segment =>  {
@@ -35,7 +38,6 @@ async function sendM(message) {
             
           };
           
-
           // Log the received data
           console.log("First Name:", data.firstName);
           console.log("Last Name:", data.lastName);
@@ -45,6 +47,7 @@ async function sendM(message) {
           console.log(data.firstName + "     " + data.lastName);
 
           resolve(data);
+        }
         }
       }
     });
