9b161e18dc8c10e0988c4b63f329b368b3edeb2f
Rawda-Yousry <rawda.ahmed02@eng-st.cu.edu.eg>
2024-05-02 05:40:53 +0300

    Added-TextField

--- client/src/modules/doctorPortal/components/diagnosisData/diagnosisData.tsx
+++ client/src/modules/doctorPortal/components/diagnosisData/diagnosisData.tsx
@@ -3,17 +3,41 @@ import classes from "../blockData/block.module.css";
 import styles from "./diagnosisData.module.css";
 import AddIcon from "@mui/icons-material/Add";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { patientActions, postText } from "../../slices/patient-slice";
 export const DiagnosisData = ({ id }: { id: string | undefined }) => {
+  const [text, setText] = useState(" ");
+  const dispatch = useDispatch();
+  const handleClick = async () => {
+    dispatch(patientActions.setText(text));
+    try {
+      await dispatch(postText(text) as any);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
   return (
     <div className={classes.block3}>
       <h2 className={classes.header}> Diagnosis Data</h2>
       <div className={styles.text}>
-        <TextField ></TextField>
-        <AddCircleIcon
-          sx={{
-            marginLeft: "7px",
+        <TextField
+          inputProps={{ style: { height: "130px" } }}
+          onChange={(e) => {
+            setText(e.target.value);
           }}
         />
+        <div
+          onClick={() => {
+            handleClick();
+          }}
+        >
+          <AddCircleIcon
+            sx={{
+              marginLeft: "7px",
+            }}
+          />
+        </div>
       </div>
       {/* // <h2>{id}</h2> */}
     </div>
